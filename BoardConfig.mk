c33ee3eaffec8220f1c9a5e53b9d70961e8f7575
klozz <xxx.reptar.rawrr.xxx@gmail.com>
2014-06-13 04:19:58 -0500

    Initial port to Moto E (Condor) Used some files from Falcon device aka Moto G
    Are Experimental only...
    
    Signed-off-by: Carlos Jesús <xxx.reptar.rawrr.xxx@gmail.com>

---
+++ BoardConfig.mk
@@ -0,0 +1,103 @@
+#
+# Copyright (C) 2013 The Android Open-Source Project
+# Copyright 2014, The AnimeROM 
+# Copyright 2014, The XPerience Project
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+TARGET_CPU_ABI		:= armeabi-v7a
+TARGET_CPU_ABI2 	:= armeabi
+TARGET_CPU_SMP 		:= true
+TARGET_ARCH 		:= arm
+TARGET_ARCH_VARIANT 	:= armv7-a-neon
+TARGET_CPU_VARIANT 	:= krait
+
+TARGET_NO_BOOTLOADER 	:= true
+
+BOARD_KERNEL_SEPARATED_DT := true
+BOARD_CUSTOM_BOOTIMG_MK   := device/moto/condor/mkbootimg.mk
+TARGET_KERNEL_SOURCE 	  := kernel/motorola/condor
+TARGET_KERNEL_CONFIG 	  := msm8610_defconfig
+BOARD_KERNEL_BASE         := 0x00000000
+BOARD_KERNEL_PAGESIZE     := 2048
+BOARD_KERNEL_CMDLINE      := console=ttyHSL0,115200,n8 androidboot.console=ttyHSL0 androidboot.hardware=qcom user_debug=31 msm_rtb.filter=0x37 utags.blkdev=/dev/block/platform/msm_sdcc.1/by-name/utags vmalloc=400M
+BOARD_MKBOOTIMG_ARGS      := --ramdisk_offset 0x01000000 --tags_offset 0x00000100
+
+TARGET_BOARD_PLATFORM_GPU    := qcom-adreno302
+TARGET_NO_RADIOIMAGE 	     := true
+TARGET_BOARD_PLATFORM 	     := msm8610
+TARGET_BOOTLOADER_BOARD_NAME := condor
+TARGET_BOARD_INFO_FILE       := device/motorola/condor/board-info.txt
+
+#TARGET_QCOM_DISPLAY_VARIANT := caf-new
+#TARGET_USES_QCOM_BSP := true
+
+BOARD_USES_QCOM_HARDWARE      := true
+
+COMMON_GLOBAL_CFLAGS += -DMOTOROLA_UIDS -DQCOM_HARDWARE
+TARGET_USES_MOTOROLA_LOG      := true
+TARGET_NR_SVC_SUPP_GIDS       := 32
+
+BOARD_EGL_CFG := device/moto/condor/egl.cfg
+
+TARGET_USES_ION     := true
+USE_OPENGL_RENDERER := true
+
+# Wifi related defines
+BOARD_HAS_QCOM_WLAN 		:= true
+BOARD_HAS_QCOM_WLAN_SDK 	:= true
+WPA_SUPPLICANT_VERSION 		:= VER_0_8_X
+BOARD_WPA_SUPPLICANT_DRIVER	:= NL80211
+BOARD_HOSTAPD_DRIVER 		:= NL80211
+BOARD_HOSTAPD_PRIVATE_LIB 	:= lib_driver_cmd_qcwcn
+BOARD_WPA_SUPPLICANT_PRIVATE_LIB := lib_driver_cmd_qcwcn
+WIFI_DRIVER_MODULE_PATH 	:= "/system/lib/modules/wlan.ko"
+WIFI_DRIVER_MODULE_NAME 	:= "wlan"
+BOARD_WLAN_DEVICE 		:= qcwcn
+TARGET_USES_WCNSS_CTRL 		:= true
+
+#Audio variant
+TARGET_QCOM_AUDIO_VARIANT 	   := caf
+BOARD_USES_ALSA_AUDIO 		   := true
+AUDIO_FEATURE_DEEP_BUFFER_PRIMARY  := true
+AUDIO_FEATURE_DYNAMIC_VOLUME_MIXER := true
+
+# Bluetooth
+BOARD_HAVE_BLUETOOTH 		:= true
+BOARD_HAVE_BLUETOOTH_QCOM 	:= true
+BOARD_BLUETOOTH_BDROID_BUILDCFG_INCLUDE_DIR := device/moto/condor/bluetooth
+
+#Revocery configs
+TARGET_USERIMAGES_USE_EXT4 	   := true
+TARGET_USERIMAGES_USE_F2FS 	   := true
+BOARD_BOOTIMAGE_PARTITION_SIZE     := 10526720
+BOARD_RECOVERYIMAGE_PARTITION_SIZE := 10526720
+BOARD_SYSTEMIMAGE_PARTITION_SIZE   := 1073741824
+BOARD_USERDATAIMAGE_PARTITION_SIZE := 29267830784
+BOARD_CACHEIMAGE_PARTITION_SIZE    := 734003200
+BOARD_CACHEIMAGE_FILE_SYSTEM_TYPE  := ext4
+BOARD_FLASH_BLOCK_SIZE 		   := 131072
+BOARD_CHARGER_DISABLE_INIT_BLANK   := true
+BOARD_USES_QC_TIME_SERVICES        := true
+
+#TARGET_RECOVERY_PIXEL_FORMAT := RGBX_8888
+#TARGET_RECOVERY_UI_LIB := librecovery_ui_condor
+TARGET_RECOVERY_FSTAB = device/motorola/condor/rootdir/fstab.qcom
+BOARD_HAS_NO_SELECT_BUTTON := true
+
+#TARGET_RELEASETOOLS_EXTENSIONS := device/moto/condor
+
+#BOARD_LIB_DUMPSTATE := libdumpstate.condor
+
+-include vendor/motorola/condor/BoardConfigVendor.mk
---
+++ anime_condor.mk
@@ -0,0 +1,20 @@
+## Specify phone tech before including full_phone
+$(call inherit-product, vendor/Anime/config/gsm.mk)
+
+# Inherit some common Anime stuff.
+$(call inherit-product, vendor/Anime/config/common.mk)
+
+# Enhanced NFC
+$(call inherit-product, vendor/Anime/config/nfc_enhanced.mk)
+
+# Inherit device configuration
+$(call inherit-product, device/motorola/condor/full_condor.mk)
+
+## Device identifier. This must come after all inclusions
+PRODUCT_DEVICE := condor
+PRODUCT_NAME := anime_condor
+PRODUCT_BRAND := motorola
+PRODUCT_MODEL := Moto E
+PRODUCT_MANUFACTURER := Motorola
+
+PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=condor_retaildsds BUILD_FINGERPRINT=motorola/condor_retaildsds/condor_umtsds:4.4.2/KXC20.82-14/13:user/release-keys PRIVATE_BUILD_DESC="condor_retaildsds-user 4.4.2 KXC20.82-14 13 release-keys"
